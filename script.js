84548cac162eef9eb7f9dc81b668dbbfa9ff3094
Admin <admin@Admins-MacBook-Air.local>
2024-07-01 16:50:35 +0530

    first commit

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function lightson() {
     const button = document.getElementById("btnon");
     const bulb = document.getElementById("off");
 
-    if (bulb.src.includes("/bulb/Broken.png")) {
+    if (bulb.src.includes("/bulb/Broken.webp")) {
         alert ("The Bulb is Broken! Press Reset");
-    }else if(bulb.src.includes("/bulb/Off.png")) {
-        bulb.src = "/bulb/On.png";
+    }else if(bulb.src.includes("/bulb/Off.webp")) {
+        bulb.src = "/bulb/On.webp";
         button.innerText = "Off";
     }else{
-        bulb.src = "/bulb/Off.png";
+        bulb.src = "/bulb/Off.webp";
         button.innerText = "On";
     }
 }
@@ -16,14 +16,14 @@ function breakbulb() {
     const broken = document.getElementById("off");
     const bulb = document.getElementById("off")
 
-    if (bulb.src.includes("/bulb/Off.png") || bulb.src.includes("/bulb/On.png")) {
-        broken.src = "/bulb/Broken.png";
+    if (bulb.src.includes("/bulb/Off.webp") || bulb.src.includes("/bulb/On.webp")) {
+        broken.src = "/bulb/Broken.webp";
     }else{
         alert("Already Broken! Press Reset");
     }
 }
 function reset() {
-    document.getElementById("off").src = "/bulb/Off.png";
+    document.getElementById("off").src = "/bulb/Off.webp";
     const reset = document.getElementById("reset");
 
 }
